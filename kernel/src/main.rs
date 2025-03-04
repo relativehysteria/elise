e5173269d26e2bfcffed9602c840e4a3d939fc83
relativehysteria <rruziro@gmail.com>
2025-03-04 23:11:04 +0100

    minimal kernel

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![allow(internal_features)]
+#![feature(lang_items)]
+
+use core::panic::PanicInfo;
+
+#[lang = "eh_personality"]
+fn eh_personality() {}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    unsafe { cpu::halt(); }
+}
+
+#[unsafe(export_name="_start")]
+extern "C" fn entry() -> ! {
+    panic!();
 }
