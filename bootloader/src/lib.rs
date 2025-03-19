40a713fecf0c9acf6520052e1a03d5bdbc151851
relativehysteria <rruziro@gmail.com>
2025-03-19 03:35:37 +0100

    forgot to uncomment

--- bootloader/src/lib.rs
+++ bootloader/src/lib.rs
@@ -16,5 +16,4 @@ pub use embedded::TRAMPOLINE;
 use oncelock::OnceLock;
 
 /// Data shared between the bootloader and the kernel
-//pub static SHARED: shared_data::Shared = shared_data::Shared::new();
 pub static SHARED: OnceLock<&'static shared_data::Shared> = OnceLock::new();
